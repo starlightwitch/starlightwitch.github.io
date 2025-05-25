7860f463e4c27b4a424d060fb20b52bbab2c7664
Jett Pavlica <jettpav@gmail.com>
2025-05-24 23:51:59 -0400

    wipe 2025

--- assets/js/activeLink.js
+++
@@ -1,7 +0,0 @@
-function addActiveClass(classSelector) {
-    let activeItem = document.querySelectorAll(classSelector);
-
-    for (let i = 0; i < activeItem.length; i++) {
-        activeItem[i].className += " active";
-    }
-}
